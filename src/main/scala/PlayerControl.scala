ca1d2812ff9e6372f324e4b0600c3461d34d0723
Konstantin Perikov <kperikov@griddynamics.com>
2015-05-16 22:55:38 +0200

    fix bug with movements

--- src/main/scala/PlayerControl.scala
+++ src/main/scala/PlayerControl.scala
@@ -7,7 +7,7 @@ case class PlayerControl(screenWidth: Int, screenHeight: Int) extends AbstractCo
   //    is the player currently moving?
   var up, down, left, right: Boolean = _
   //    speed of the player
-  var speed = 50f
+  var speed = 150f
   //    lastRotation of the player
   var lastRotation: Float = _
 
@@ -24,13 +24,13 @@ case class PlayerControl(screenWidth: Int, screenHeight: Int) extends AbstractCo
       lastRotation = FastMath.PI / 2
     } else if (down) {
       if (spatial.getLocalTranslation().y > spatial.getUserData[Int]("radius")) {
-        spatial.move(0, tpf * speed, 0)
+        spatial.move(0, -tpf * speed, 0)
       }
       spatial.rotate(0, 0, -lastRotation + FastMath.PI * 1.5f);
       lastRotation = FastMath.PI * 1.5f
     } else if (left) {
       if (spatial.getLocalTranslation().x > spatial.getUserData[Int]("radius")) {
-        spatial.move(tpf * speed, 0, 0)
+        spatial.move(-tpf * speed, 0, 0)
       }
       spatial.rotate(0, 0, -lastRotation + FastMath.PI)
       lastRotation = FastMath.PI
