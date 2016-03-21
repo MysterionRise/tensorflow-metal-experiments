7bd1887702e20c01ba54a7e42ca089f4e083f927
Konstantin Perikov <MysterionRise@users.noreply.github.com>
2016-03-21 17:57:58 +0100

    Update MyFirstGame.scala

--- src/main/scala/org/mystic/MyFirstGame.scala
+++ src/main/scala/org/mystic/MyFirstGame.scala
@@ -1,101 +1,461 @@
 package org.mystic
 
-import com.jme3.asset.AssetManager
-import com.jme3.font.BitmapText
-import com.jme3.scene.Node
+import java.awt.GraphicsEnvironment
+import java.io.{PrintWriter, BufferedReader, File, FileReader}
+import java.util.concurrent.TimeUnit
+import com.jme3.cursors.plugins.JmeCursor
+import com.jme3.system.AppSettings
 
-class Hud(assetManager: AssetManager, guiNode: Node, width: Int, height: Int) {
+import scala.collection.JavaConversions._
+import com.jme3.app.SimpleApplication
+import com.jme3.input.controls.{ActionListener, AnalogListener, KeyTrigger, MouseButtonTrigger}
+import com.jme3.input.{KeyInput, MouseInput}
+import com.jme3.material.Material
+import com.jme3.material.RenderState.BlendMode
+import com.jme3.math.{FastMath, Vector2f, Vector3f}
+import com.jme3.post.FilterPostProcessor
+import com.jme3.post.filters.BloomFilter
+import com.jme3.scene.{Node, Spatial}
+import com.jme3.texture.Texture2D
+import com.jme3.ui.Picture
+import org.mystic.controls._
+import org.mystic.Utils._
 
-  def reset = {
-    lives = 1
-    score = 0
+import scala.collection.immutable.HashSet
+import scala.util.Random
+
+object MyFirstGame extends SimpleApplication with ActionListener with AnalogListener {
+
+  private var player: Spatial = _
+  val Alive = "alive"
+  val Radius = "radius"
+  val DieTime: String = "dieTime"
+  private val Left = "left"
+  private val Right = "right"
+  private val Up = "up"
+  private val Down = "down"
+  private val MouseClick = "mouseClick"
+
+  private var bulletCooldown: Long = 0
+  private var bulletNode: Node = _
+  private var enemyNode: Node = _
+
+  private var gameOver = false
+
+  private var enemySpawnCooldown: Long = _
+  private var enemySpawnChance: Float = 80
+
+  private var blackHoleCooldown: Long = 0
+  private var blackHoleNode: Node = _
+
+  private var extraLifeNode: Node = _
+
+  private var sound: SoundManager = _
+
+  private var hud: Hud = _
+
+  override def simpleInitApp(): Unit = {
+    // create sounds manager
+    sound = new SoundManager(assetManager)
+    sound.startMusic
+    // set up camera for 2D
+    cam.setParallelProjection(true)
+    cam.setLocation(new Vector3f(0, 0, 10.0f))
+    getFlyByCamera().setEnabled(false)
+
+    // turn off stats view
+    setDisplayStatView(false)
+    setDisplayFps(false)
+
+    // add listener for keyboard
+    inputManager.addMapping(Left, new KeyTrigger(KeyInput.KEY_LEFT))
+    inputManager.addMapping(Right, new KeyTrigger(KeyInput.KEY_RIGHT))
+    inputManager.addMapping(Up, new KeyTrigger(KeyInput.KEY_UP))
+    inputManager.addMapping(Down, new KeyTrigger(KeyInput.KEY_DOWN))
+    inputManager.addListener(this, Left)
+    inputManager.addListener(this, Right)
+    inputManager.addListener(this, Up)
+    inputManager.addListener(this, Down)
+
+    // add listener for mouse
+    inputManager.addMapping(MouseClick, new MouseButtonTrigger(MouseInput.BUTTON_LEFT))
+    inputManager.addListener(this, MouseClick)
+
+    //        setup the bulletNode
+    bulletNode = new Node("bullets")
+    guiNode.attachChild(bulletNode)
+
+    // set up the enemyNode
+    enemyNode = new Node("enemies")
+    guiNode.attachChild(enemyNode)
+
+    blackHoleNode = new Node("blackholes")
+    guiNode.attachChild(blackHoleNode)
+
+    extraLifeNode = new Node("extralifes")
+    guiNode.attachChild(extraLifeNode)
+
+    // add player
+    player = getSpatial("Player")
+    player.setUserData(Alive, true)
+    player.move(settings.getWidth() / 2, settings.getHeight() / 2, 0)
+    guiNode.attachChild(player)
+    player.addControl(new PlayerControl(settings.getWidth(), settings.getHeight()))
+
+    inputManager.setMouseCursor(assetManager.loadAsset("Textures/Pointer.ico").asInstanceOf[JmeCursor])
+
+    hud = new Hud(assetManager, guiNode, settings.getWidth(), settings.getHeight())
+    hud.loadFont
+    hud.reset
+
+    // add bloom filter
+    val fpp = new FilterPostProcessor(assetManager)
+    val bloom = new BloomFilter()
+    fpp.addFilter(bloom)
+    guiViewPort.addProcessor(fpp)
+    guiViewPort.setClearColor(true)
   }
 
-  def addPoint = score += 1
-
-  def removeLife = lives -= 1
-
-  def addLife = lives += 1
-
-  def loadFont = {
-    val guiFont = assetManager.loadFont("Interface/output-saucerbb-2048.fnt")
-    livesText = new BitmapText(guiFont, false)
-    livesText.setLocalTranslation(30, height - 30, 0)
-    livesText.setSize(fontSize)
-    livesText.setText("LIVES: " + lives)
-    guiNode.attachChild(livesText)
-
-    scoreText = new BitmapText(guiFont, true)
-    scoreText.setLocalTranslation(width - 200, height - 30, 0)
-    scoreText.setSize(fontSize)
-    scoreText.setText("SCORE: " + score)
-    guiNode.attachChild(scoreText)
+  // entry point of the game
+  def main(args: Array[String]): Unit = {
+    val mySettings = new AppSettings(true)
+    mySettings.setTitle("Neon shooter")
+    mySettings.setSettingsDialogImage("Interface/splashscreen.png")
+    //    val device = GraphicsEnvironment.getLocalGraphicsEnvironment.getDefaultScreenDevice
+    //    val modes = device.getDisplayModes
+    //    val widths = new HashSet[Int]
+    //    val heights = new HashSet[Int]
+    //    val refreshRate = new HashSet[Int]
+    //    val bitDepths = new HashSet[Int]
+    //    modes.foreach(x => {
+    //      widths + x.getWidth
+    //      heights + x.getHeight
+    //      refreshRate + x.getRefreshRate
+    //      bitDepths + x.getBitDepth
+    //      println(s"${x.getWidth} ${x.getHeight} ${x.getRefreshRate} ${x.getBitDepth}")
+    //    })
+    //    val lastMode = modes.length - 1
+    //    mySettings.setResolution(modes(lastMode).getWidth, modes(lastMode).getHeight)
+    //    mySettings.setFrequency(modes(lastMode).getRefreshRate)
+    //    mySettings.setDepthBits(modes(lastMode).getBitDepth)
+    //    mySettings.setFullscreen(device.isFullScreenSupported)
+    //    MyFirstGame.setShowSettings(false)
+    MyFirstGame.setSettings(mySettings)
+    MyFirstGame.start()
   }
 
-  def showBeatingHighScoreMessage = {
-    // todo create something
+  private def getSpatial(name: String): Spatial = {
+    val node = new Node(name)
+    // load picture
+    val pic = new Picture(name)
+    val tex: Texture2D = assetManager.loadTexture(s"Textures/${name}.png").asInstanceOf[Texture2D]
+    pic.setTexture(assetManager, tex, true)
+
+    // adjust picture
+    val width = tex.getImage.getWidth
+    val height = tex.getImage.getHeight
+    pic.setWidth(width)
+    pic.setHeight(height)
+    // for rotation purposes
+    pic.move(-width / 2f, -height / 2f, 0)
+
+    // add a material to the picture
+    val pictureMaterial = new Material(assetManager, "Common/MatDefs/Gui/Gui.j3md")
+    pictureMaterial.getAdditionalRenderState.setBlendMode(BlendMode.AlphaAdditive)
+    node.setMaterial(pictureMaterial)
+
+    // set the radius of the spatial
+    // (using width only as a simple approximation)
+    node.setUserData(Radius, (width / 2.0f))
+
+    // attach the picture to the node and return it
+    node.attachChild(pic)
+    node
   }
 
-  /**
-    * public void endGame() {
-    // init gameOverNode
-    gameOverNode = new Node();
-    gameOverNode.setLocalTranslation(screenWidth/2 - 180, screenHeight/2 + 100,0);
-    guiNode.attachChild(gameOverNode);
-
-    // check highscore
-    int highscore = loadHighscore();
-    if (score > highscore) {saveHighscore();}
-
-    // init and display text
-    BitmapText gameOverText = new BitmapText(guiFont, false);
-    gameOverText.setLocalTranslation(0,0,0);
-    gameOverText.setSize(fontSize);
-    gameOverText.setText("Game Over");
-    gameOverNode.attachChild(gameOverText);
-
-    BitmapText yourScoreText = new BitmapText(guiFont, false);
-    yourScoreText.setLocalTranslation(0,-50,0);
-    yourScoreText.setSize(fontSize);
-    yourScoreText.setText("Your Score: "+score);
-    gameOverNode.attachChild(yourScoreText);
-
-    BitmapText highscoreText = new BitmapText(guiFont, false);
-    highscoreText.setLocalTranslation(0,-100,0);
-    highscoreText.setSize(fontSize);
-    highscoreText.setText("Highscore: "+highscore);
-    gameOverNode.attachChild(highscoreText);
-}
-    * @return
-    */
-
-  def endGame = {
-    guiNode.detachAllChildren()
-    val gameOverNode = new Node()
-    gameOverNode.setLocalTranslation(width / 2 - 180, height / 2 + 100, 0)
-    guiNode.attachChild(gameOverNode)
-//    val highscore = loadHighscore()
-//    if (score > highscore) saveHighscore()
-
-    val guiFont = assetManager.loadFont("Interface/output-saucerbb-2048.fnt")
-    endGameResponse = new BitmapText(guiFont, false)
-    endGameResponse.setLocalTranslation(0, 0, 0)
-    endGameResponse.setSize(fontSize * 2)
-    endGameResponse.setText("GAME OVER")
-    gameOverNode.attachChild(endGameResponse)
+  override def onAction(name: String, isPressed: Boolean, tpf: Float): Unit = {
+    checkSpatialIsAlive(player, () => {
+      name match {
+        // TODO change it to getControl by class
+        case Up => player.getControl(0).asInstanceOf[PlayerControl].up = isPressed
+        case Down => player.getControl(0).asInstanceOf[PlayerControl].down = isPressed
+        case Left => player.getControl(0).asInstanceOf[PlayerControl].left = isPressed
+        case Right => player.getControl(0).asInstanceOf[PlayerControl].right = isPressed
+        case _ =>
+      }
+    }, () => {})
   }
 
-  val fontSize = 35
-  var livesText: BitmapText = _
-  var scoreText: BitmapText = _
-  var endGameResponse: BitmapText = _
+  def getAimDirection: Vector3f = {
+    val mouse = inputManager.getCursorPosition
+    val playerPosition = player.getLocalTranslation
+    val difference = new Vector3f(mouse.x - playerPosition.x, mouse.y - playerPosition.y, 0)
+    difference.normalizeLocal
+  }
 
-  var lives = 1
-  var score = 0
+  def launchTwoBullets = {
+    if (System.currentTimeMillis() - bulletCooldown > 83f) {
+      sound.shoot
+      bulletCooldown = System.currentTimeMillis
+      val aim = getAimDirection
+      val offset = new Vector3f(aim.y / 3, -aim.x / 3, 0)
+
+      //                    init bullet 1
+      val bullet = getSpatial("Bullet")
+      var finalOffset = aim.add(offset).mult(30)
+      var trans = player.getLocalTranslation().add(finalOffset)
+      bullet.setLocalTranslation(trans)
+      bullet.addControl(new BulletControl(aim, settings.getWidth(), settings.getHeight()))
+      bulletNode.attachChild(bullet)
+
+      //                    init bullet 2
+      val bullet2 = getSpatial("Bullet")
+      finalOffset = aim.add(offset.negate()).mult(30)
+      trans = player.getLocalTranslation().add(finalOffset)
+      bullet2.setLocalTranslation(trans)
+      bullet2.addControl(new BulletControl(aim, settings.getWidth(), settings.getHeight()))
+      bulletNode.attachChild(bullet2)
+    }
+  }
+
+  override def onAnalog(name: String, value: Float, tpf: Float): Unit = {
+    checkSpatialIsAlive(player, () => {
+      name match {
+        case MouseClick => launchTwoBullets
+        case _ =>
+      }
+    }, () => {})
+  }
+
+  def getSpawnPosition: Vector3f = {
+    var pos: Vector3f = new Vector3f(new Random().nextInt(settings.getWidth()), new Random().nextInt(settings.getHeight()), 0)
+    do {
+      pos = new Vector3f(new Random().nextInt(settings.getWidth()), new Random().nextInt(settings.getHeight()), 0)
+    } while (pos.distanceSquared(player.getLocalTranslation()) < 8000)
+    pos
+  }
 
 
-  def updateHUD = {
-    scoreText.setText("SCORE: " + score)
-    livesText.setText("LIVES: " + lives)
+  def createSeeker = {
+    val seeker = getSpatial("Seeker")
+    seeker.setLocalTranslation(getSpawnPosition)
+    seeker.addControl(new SeekerControl(player))
+    seeker.setUserData("active", false)
+    enemyNode.attachChild(seeker)
+  }
+
+
+  def createWanderer = {
+    val wanderer = getSpatial("Wanderer")
+    wanderer.setLocalTranslation(getSpawnPosition)
+    wanderer.addControl(new WandererControl(settings.getWidth, settings.getHeight))
+    wanderer.setUserData("active", false)
+    enemyNode.attachChild(wanderer)
+  }
+
+  def createBlackHole = {
+    val blackHole = getSpatial("BlackHole")
+    blackHole.setLocalTranslation(getSpawnPosition)
+    blackHole.addControl(new BlackHoleControl())
+    blackHole.setUserData("active", false)
+    blackHoleNode.attachChild(blackHole)
+  }
+
+  def spawnEnemies = {
+    if (System.currentTimeMillis() - enemySpawnCooldown >= 17) {
+      enemySpawnCooldown = System.currentTimeMillis
+      //      sound.spawn
+
+      if (enemyNode.getQuantity < 50) {
+        if (new Random().nextInt(enemySpawnChance.toInt) == 0) {
+          createSeeker
+        }
+        if (new Random().nextInt(enemySpawnChance.toInt) == 0) {
+          createWanderer
+        }
+      }
+      //increase Spawn Time
+      if (enemySpawnChance >= 1.1f) {
+        enemySpawnChance -= 0.005f
+      }
+    }
+  }
+
+  def spawnBlackHoles = {
+    if (blackHoleNode.getQuantity < 5 && System.currentTimeMillis - blackHoleCooldown > 5000 && new Random().nextInt(1000) == 1) {
+      blackHoleCooldown = System.currentTimeMillis
+      createBlackHole
+    }
+  }
+
+  def createExtraLife = {
+    val extraLife = getSpatial("ExtraLife")
+    extraLife.setLocalTranslation(getSpawnPosition)
+    extraLife.addControl(new ExtraLifeControl())
+    extraLife.setUserData("active", false)
+    extraLifeNode.attachChild(extraLife)
+  }
+
+  def spawnExtraLife = {
+    if (extraLifeNode.getQuantity == 0 && new Random().nextInt(100) == 1) {
+      createExtraLife
+    }
+  }
+
+
+  def checkCollision(a: Spatial, b: Spatial): Boolean = {
+    val distance = a.getLocalTranslation().distance(b.getLocalTranslation())
+    val maxDistance = a.getUserData[Float](Radius) + b.getUserData[Float](Radius)
+    distance <= maxDistance
+  }
+
+  def killPlayer = {
+    player.removeFromParent()
+    player.getControl(0).asInstanceOf[PlayerControl].reset()
+    player.setUserData(Alive, false)
+    player.setUserData(DieTime, System.currentTimeMillis)
+    enemyNode.detachAllChildren
+    blackHoleNode.detachAllChildren
+    extraLifeNode.detachAllChildren
+  }
+
+  def handleCollisions = {
+    // should the player die?
+    enemyNode.getChildren.foreach(enemy => {
+      checkSpatialIsAlive(enemy, () => {
+        if (checkCollision(player, enemy))
+          killPlayer
+      }, () => {})
+      bulletNode.getChildren.foreach(bullet => {
+        if (checkCollision(enemy, bullet)) {
+          sound.explosion
+          enemyNode.detachChild(enemy)
+          bulletNode.detachChild(bullet)
+          hud.addPoint
+        }
+      })
+    })
+    blackHoleNode.getChildren.foreach(blackHole => {
+      if (checkCollision(blackHole, player)) {
+        killPlayer
+      }
+      enemyNode.getChildren.foreach(enemy => {
+        if (checkCollision(blackHole, enemy)) {
+          sound.explosion
+          enemyNode.detachChild(enemy)
+        }
+      })
+      bulletNode.getChildren.foreach(bullet => {
+        if (checkCollision(blackHole, bullet)) {
+          val control: BlackHoleControl = blackHole.getControl(0).asInstanceOf[BlackHoleControl]
+          control.takeShot
+          bulletNode.detachChild(bullet)
+          if (control.isDead) {
+            blackHoleNode.detachChild(blackHole)
+            sound.explosion
+          }
+
+        }
+      })
+    })
+    extraLifeNode.getChildren.foreach(extra => {
+      if (checkCollision(extra, player)) {
+        sound.extraLife
+        extraLifeNode.detachChild(extra)
+        hud.addLife
+        // add extra life to player
+      }
+    })
+  }
+
+  def isNear(a: Spatial, b: Spatial): Boolean = {
+    val v1 = a.getLocalTranslation
+    val v2 = b.getLocalTranslation
+    // TODO change constant depending on size of screen
+    v1.distanceSquared(v2) <= 700f * 700f
+  }
+
+  def applyGravity(blackHole: Spatial, target: Spatial, tpf: Float) = {
+    val gravity = blackHole.getLocalTranslation.subtract(target.getLocalTranslation)
+    val distance = gravity.length
+    gravity.divideLocal(distance * distance)
+
+    target.getName match {
+      case "Player" => target.getControl(0).asInstanceOf[PlayerControl].applyGravity(gravity.multLocal(150f))
+      case "Bullet" => target.getControl(0).asInstanceOf[BulletControl].applyGravity(gravity.multLocal(-0.99f))
+      case "Seeker" => target.getControl(0).asInstanceOf[SeekerControl].applyGravity(gravity.multLocal(40000f))
+      case "Wanderer" => target.getControl(0).asInstanceOf[WandererControl].applyGravity(gravity.multLocal(40000f))
+    }
+  }
+
+  def createGravity(tpf: Float): Unit = {
+    blackHoleNode.getChildren.foreach(blackHole => {
+      if (isNear(blackHole, player)) {
+        applyGravity(blackHole, player, tpf)
+      }
+      bulletNode.getChildren.foreach(bullet => {
+        if (isNear(blackHole, bullet)) {
+          applyGravity(blackHole, bullet, tpf)
+        }
+      })
+      enemyNode.getChildren.foreach(enemy => {
+        if (isNear(blackHole, enemy)) {
+          applyGravity(blackHole, enemy, tpf)
+        }
+      })
+    })
+  }
+
+  def loadHighScore: Int = {
+    try {
+      val reader = new FileReader(new File("highscore"))
+      val buff = new BufferedReader(reader)
+      return Integer.valueOf(buff.readLine())
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
+    return 0
+  }
+
+  def saveHighScore(score: Int) = {
+    try {
+      val writer = new PrintWriter(new File("highscore"))
+      writer.write(String.valueOf(score))
+      writer.close()
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
+  }
+
+  override def simpleUpdate(tpf: Float): Unit = {
+    checkSpatialIsAlive(player, () => {
+      spawnEnemies
+      spawnBlackHoles
+      handleCollisions
+      spawnExtraLife
+      createGravity(tpf)
+      hud.updateHUD
+    }, () => {
+      if (System.currentTimeMillis() - player.getUserData(DieTime).asInstanceOf[Long] > 4000f && !gameOver) {
+        if (hud.lives > 0) {
+          hud.removeLife
+        } else {
+          hud.endGame
+          gameOver = true
+          // game ends
+          val highScore = loadHighScore
+          if (hud.score > highScore) {
+            println("You beat it!")
+            // todo show some message
+            hud.showBeatingHighScoreMessage
+            saveHighScore(hud.score)
+          }
+          //          hud.reset
+        }
+        player.setLocalTranslation(settings.getWidth() / 2, settings.getHeight() / 2, 0)
+        guiNode.attachChild(player)
+        player.setUserData(Alive, true)
+      }
+    })
   }
 }
