9ce765c70b2ac889b067058c36300b514c4b8528
Konstantin Perikov <MysterionRise@users.noreply.github.com>
2016-03-22 17:29:44 +0100

    Update ParticleManager.scala

--- src/main/scala/org/mystic/ParticleManager.scala
+++ src/main/scala/org/mystic/ParticleManager.scala
@@ -8,6 +8,36 @@ import scala.util.Random
 
 class ParticleManager(guiNode: Node, standardParticle: Spatial, glowParticle: Spatial) {
 
+  def playerExplosion(loc: Vector3f) = {
+    val color1 = ColorRGBA.Yellow
+    val color2 = ColorRGBA.Red
+    for (i <- 0 until 2000) {
+
+      val velocity = getRandomVelocity(1500)
+      val particle = standardParticle.clone()
+      particle.setLocalTranslation(loc)
+      val color = new ColorRGBA()
+      color.interpolate(color1, color2, random.nextFloat)
+      particle.addControl(new ParticleControl(velocity, 3000, color))
+      particle.setUserData("affectedByGravity", true)
+      particleNode.attachChild(particle)
+    }
+  }
+
+  def bulletExplosion(loc: Vector3f) = {
+    for (i <- 0 until 20) {
+
+      val velocity = getRandomVelocity(500)
+      val particle = standardParticle.clone()
+      particle.setLocalTranslation(loc)
+      val color = new ColorRGBA(0.676f, 0.844f, 0.898f, 1)
+      particle.addControl(new ParticleControl(velocity, 1000, color))
+      particle.setUserData("affectedByGravity", true)
+      particleNode.attachChild(particle)
+    }
+  }
+
+
   var particleNode: Node = new Node("particles")
   guiNode.attachChild(particleNode)
   val random = new Random()
@@ -26,6 +56,7 @@ class ParticleManager(guiNode: Node, standardParticle: Spatial, glowParticle: Sp
       val color = new ColorRGBA()
       color.interpolate(color1, color2, random.nextFloat() * 0.5f)
       particle.addControl(new ParticleControl(velocity, 3100, color))
+      particle.setUserData("affectedByGravity", true)
       particleNode.attachChild(particle)
     }
   }
